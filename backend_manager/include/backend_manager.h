68bbb688295788b6be598c0227ac66f58342bc51
Kirill Livanov <KirushaLivanov@gmail.com>
2019-05-24 23:13:51 +0300

    Added Backend Manager

---
+++ backend_manager/include/backend_manager.h
@@ -0,0 +1,10 @@
+#ifndef BACKEND_MANAGER_INCLUDE_BACKEND_MANAGER_H_
+#define BACKEND_MANAGER_INCLUDE_BACKEND_MANAGER_H_
+
+namespace backend_manager {
+
+class BackendManager {};
+
+}  // namespace backend_manager
+
+#endif  // BACKEND_MANAGER_INCLUDE_BACKEND_MANAGER_H_
---
+++ backend_manager/tests/main.cpp
@@ -0,0 +1,3 @@
+#include "backend_manager.h"
+
+int main() { return 0; }
